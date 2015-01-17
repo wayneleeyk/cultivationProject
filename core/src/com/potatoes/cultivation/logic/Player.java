afcb3ee7b96184f5d4648724746922ec4ca17241
rudolf <rudolf.lam@mail.mcgill.ca>
2015-01-16 20:26:39 -0500

    Added placeholders

---
+++ core/src/com/potatoes/cultivation/logic/Player.java
@@ -0,0 +1,5 @@
+package com.potatoes.cultivation.logic;
+
+public class Player {
+
+}
--- core/src/com/potatoes/cultivation/networking/Client.java
+++ core/src/com/potatoes/cultivation/networking/Client.java
@@ -1,5 +1,14 @@
 package com.potatoes.cultivation.networking;
 
-public class Client {
+import java.util.LinkedList;
+import java.util.List;
 
+import com.potatoes.cultivation.logic.Command;
+import com.potatoes.cultivation.logic.Player;
+
+public class Client {
+	public void login(String username, String password){}
+	public void createAccount(String username, String password){}
+	public List<Player> updateLobby(){return new LinkedList<Player>();}
+	public void endTurn(List<Command> actions){}
 }
