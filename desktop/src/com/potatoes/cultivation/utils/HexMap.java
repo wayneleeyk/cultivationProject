3460a86e1a8e5416317c5b665d9ec58faa61e25c
Shivani <Shivani@Shivani-PC>
2015-01-24 17:36:59 -0500

    The hexMap file creator

---
+++ desktop/src/com/potatoes/cultivation/utils/HexMap.java
@@ -0,0 +1,101 @@
+package com.potatoes.cultivation.utils;
+import java.util.Random;
+
+public class HexMap {
+	
+	//keeps track of the height and width of out hex map
+	private int width;
+	private int height;
+	
+	//private String mapShape;
+	public String [][] array;
+	
+	//constructor
+	public HexMap (int userW , int userH ){ 
+		width = userW;
+		height= userH;
+		createMap();
+		addRandomTiles();
+	}
+	
+	//Initializes the array representation of the map
+	private void createMap(){
+		array = new String [width][height];
+		
+	}
+	
+	private void addRandomTiles(){
+		// determine how many trees and meadows are needed
+		int numTrees = (int)(Math.round(height * width * .2));
+		int numMeadow = (int)(Math.round(height * width * .1));
+		int column = 0;
+		int row = 0;
+		//counter
+		int i = 0;
+		
+		//first we have to surround the border with sea tiles
+		//start with entire first row
+		while(i < width){
+			array[i][0] = "sea";
+			i++;
+		}
+		//reset counter and now fill the first column
+		i = 0;
+		while(i < height){
+			array[0][i] = "sea";
+			i++;
+		}
+		//reset the counter and now fill the last column
+		i = 0;
+		while(i < height ){
+			array[width-1][i] = "sea";
+			i++;
+		}
+		//reset the counter and now fill the last row
+		i = 0;
+		while(i < width ){
+			array[i][height-1] = "sea";
+			i++;
+		}
+		//reset counter
+		i = 0;
+		
+		//randomly insert said amount of trees and meadows into the 2D array map
+		//First create random number generator
+		Random rand = new Random();
+		//insert trees
+		while (i < numTrees){
+			column = rand.nextInt(width);
+			row = rand.nextInt(height);
+			//only move on to setting the next tile if the tile is unnamed
+			if(array[column][row] == null){
+				array[column][row] = "tree";
+				i++;
+			}
+		}
+			
+			//reset the counter
+			i = 0;
+		// now add the meadow tiles, same procedure but with meadows
+			while (i < numMeadow){
+				column = rand.nextInt(width);
+				row = rand.nextInt(height);
+				//only move on to setting the next tile if the tile is unnamed
+				if(array[column][row] == null){
+					array[column][row] = "meadow";
+					i++;
+				}
+			}
+			
+			// change all the "null" spaces to read as grass
+			for (int a = 0; a < width; a++){
+				for (int b = 0; b < height; b++){
+					if(array[a][b] == null){
+						array[a][b] = "grass";
+					}
+				}
+			}
+	}
+	
+}
+
---
+++ desktop/src/com/potatoes/cultivation/utils/HexMapFileCreator.java
@@ -0,0 +1,52 @@
+//Run this file to create a map representation of the tiles. A txt file will be created in the working directory.
+package com.potatoes.cultivation.utils;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class HexMapFileCreator {
+	
+	public static void main (String[] arg) {
+		
+		//prompt user for input
+		Scanner user_input = new Scanner( System.in );
+		System.out.println("Enter width: ");
+		int width = user_input.nextInt();
+		System.out.println("Enter height: ");
+		int height = user_input.nextInt();
+		user_input.close();
+		
+		//create the hexMap
+		HexMap map = new HexMap(width,height);
+		//map.createMap();
+		//map.addRandomTiles();
+		
+		//now Let's print it to a file!
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter("hexMapFile.txt", "UTF-8");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < width ; i++){
+			
+			for (int j = 0; j < height ; j++){
+				writer.print(map.array[i][j] + ", ");
+			}
+			writer.println();
+		}
+		
+		writer.close();
+		
+		
+	}
+	
+	
+
+}
