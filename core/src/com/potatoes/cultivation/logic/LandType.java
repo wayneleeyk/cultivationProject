a574e94fcfc72cf32e7d7d474d3322eb34bc51e4
Mang0-Tang0 <shivani300@gmail.com>
2015-02-27 13:24:09 -0500

    LandType Done
    just had one method for determining gold worth.

--- core/src/com/potatoes/cultivation/logic/LandType.java
+++ core/src/com/potatoes/cultivation/logic/LandType.java
@@ -1,10 +1,27 @@
 package com.potatoes.cultivation.logic;
 
+//Author: Shivani Sharma
+
 public enum LandType {
 
 	Sea, Tree, Grass, Meadow;
 	
+	/* Sea or Tree Tile: No money awarded
+	 * Grass (empty tile) : +1g
+	 * Meadow: +2g
+	 */
 	public int getGoldValue(LandType type){
-		return 0;
+		if(type.ordinal() == 0 || type.ordinal() == 1){
+			return 0;
+		}
+		else if(type.ordinal() == 2){
+			return 1;
+		}
+		else if(type.ordinal() == 3){
+			return 2;
+		}
+		else{
+			return -1;
+		}
 	}
 }
