957d514cc30a3a536f59800d10f627d4feb7da9e
rudolf <rudolf.lam@mail.mcgill.ca>
2015-03-11 16:15:06 -0400

    New and improved

--- core/src/com/potatoes/cultivation/networking/ProtocolHandler.java
+++ core/src/com/potatoes/cultivation/networking/ProtocolHandler.java
@@ -1,5 +1,16 @@
 package com.potatoes.cultivation.networking;
 
-public interface ProtocolHandler {
-	public void handle(Protocol p);
+public abstract class ProtocolHandler <T> {
+	private T result = null;
+	public abstract void handle(Protocol p);
+	public T getResult(){
+		while(result == null){
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
 }
