2db3ced338f181122f43952afb396aaeeba6f67c
Wayne Lee <wayneleeyk@users.noreply.github.com>
2015-03-12 01:43:22 -0400

    hud updated

--- core/src/com/potatoes/cultivation/screens/HUD.java
+++ core/src/com/potatoes/cultivation/screens/HUD.java
@@ -1,7 +1,6 @@
 package com.potatoes.cultivation.screens;
 
-
-import sun.awt.X11.Screen;
+import java.util.ArrayList;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -21,7 +20,9 @@ import com.potatoes.cultivation.logic.Region;
 import com.potatoes.cultivation.logic.Tile;
 import com.potatoes.cultivation.logic.Village;
 import com.potatoes.cultivation.screens.InGame.VillageImage;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 
 public class HUD extends Stage{
 	
@@ -35,6 +36,7 @@ public class HUD extends Stage{
 	String villageType;
 	Cultivation game;
 	Skin skin;
+	ArrayList<Actor> destroyableMenus = new ArrayList<Actor>();
 	
 	public HUD(Cultivation game, SpriteBatch batch, GameMap map, Player currentPlayer) {
 		
@@ -49,6 +51,22 @@ public class HUD extends Stage{
 		this.goldCount = 0;
 		this.skin = game.skin;
 		System.out.println(this.width/2 + " " + this.height/2);
+		
+		
+		this.addListener(new ClickListener() {
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+				System.out.println("HUD clicked at " + x + "," + y);
+				Actor hit = HUD.this.hit(x, y, true);
+				
+				if(hit != null) return true;
+				for(Actor a : destroyableMenus) {
+					a.remove();
+				}
+				return false;
+			}
+		});
+		
 	}
 
 	@Override
@@ -86,35 +104,43 @@ public class HUD extends Stage{
 		System.out.println("CLICKED VILLAGE");
 		final Group villageMenuGroup = new Group();
 		this.addActor(villageMenuGroup);
+		this.destroyableMenus.add(villageMenuGroup);
+		
 		System.out.println("clicked (" + x + ", " + y + ")");		
 		final TextButton upgradeVillage = new TextButton("Upgrade Village", skin, "default");
-		upgradeVillage.setPosition(x, Gdx.graphics.getHeight() -y);
-		upgradeVillage.setVisible(true);
-		upgradeVillage.addListener(new ClickListener() {
-			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				System.out.println("Button pressed");
+		final TextButton hireVillager = new TextButton("Hire Villager", skin, "default");
+		
+		upgradeVillage.setPosition(x, Gdx.graphics.getHeight() - y);
+		upgradeVillage.addListener(new ChangeListener() {
+			public void changed(ChangeEvent event, Actor actor) {
+				System.out.println("upgradeVillage clicked");
 				GameAction.UpgradeVillageAction gameAction = new GameAction.UpgradeVillageAction(villageImage.getVillage());
 				gameAction.execute(game.GAMEMANAGER.getGame());
 				villageImage.updateVillageSprite();
-				return true;
 			}
 		});
 		
-		villageMenuGroup.addActor(upgradeVillage);
-	
-		villageMenuGroup.getStage().addListener(new ClickListener() {
-			//Listener that destroys village menu buttons when clicked anywhere except buttons
+		hireVillager.setPosition(x + 48, Gdx.graphics.getHeight() - y - 50);
+		hireVillager.addListener(new ChangeListener() {
 			@Override
-			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				System.out.println("HUD clicked at " + x + "," + y);
-//				Actor hit = HUD.this.hit(x, y, true);
-//				System.out.println("Actor hit:" + hit);
-				villageMenuGroup.removeActor(upgradeVillage);
-				villageMenuGroup.getStage().removeListener(this);
-				return false;
+			public void changed(ChangeEvent event, Actor actor) {
+				System.out.println("hireVillager clicked");
 			}
 		});
-	}
-	
+		
+		
+		villageMenuGroup.addActor(upgradeVillage);
+		villageMenuGroup.addActor(hireVillager);
+		
+		villageMenuGroup.addListener(new ChangeListener(){
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				System.out.println("menu group clicked");
+				for(Actor a : destroyableMenus) {
+					if(a.equals(villageMenuGroup)) continue;
+					a.remove();
+				}
+			}
+		});
+	}	
 }
