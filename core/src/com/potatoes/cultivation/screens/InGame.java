ee3f08976bd10417d84ae2a6a5851b54b47c3a86
Monica <kiwiful@gmail.com>
2015-03-12 01:58:38 -0400

    Fixed dragging bug

--- core/src/com/potatoes/cultivation/screens/InGame.java
+++ core/src/com/potatoes/cultivation/screens/InGame.java
@@ -38,7 +38,7 @@ import com.potatoes.cultivation.logic.UnitType;
 import com.potatoes.cultivation.logic.Village;
 import com.potatoes.cultivation.logic.VillageType;
 
-public class InGame extends ScreenAdapter{
+public class InGame extends ScreenAdapter {
 
 	Cultivation game;
 	SpriteBatch batch;
@@ -50,26 +50,28 @@ public class InGame extends ScreenAdapter{
 	AtlasRegion village_hovel;
 	AtlasRegion village_town;
 	AtlasRegion village_fort;
-	float cameraWidth=800, cameraHeight=600;
-	Camera camera = new OrthographicCamera(cameraWidth,cameraHeight);
+	float cameraWidth = 800, cameraHeight = 600;
+	Camera camera = new OrthographicCamera(cameraWidth, cameraHeight);
 	Stage gameStage = new Stage();
-	HUD hud;	
+	HUD hud;
 	GameMap gameMap;
 	Set<Tile> tiles = new HashSet<>();
 	List<Color> colorByIndex;
-	
+
 	public InGame(final Cultivation pGame, CultivationGame aGameRound) {
 		this.game = pGame;
 		this.batch = game.batch;
-		this.hud = new HUD(this.game, this.batch, this.gameMap, this.game.player);
+		this.hud = new HUD(this.game, this.batch, this.gameMap,
+				this.game.player);
 		this.atlas = game.manager.get("ingame.atlas", TextureAtlas.class);
 		this.gameMap = aGameRound.getGameMap();
-		//Assigns a unique color to each player to colour the tiles that they own
+		// Assigns a unique color to each player to colour the tiles that they
+		// own
 		colorByIndex = new ArrayList<Color>();
-		colorByIndex.add(new Color(1,0,0,0.99f));
-		colorByIndex.add(new Color(0,1,1,0.7f));
-		colorByIndex.add(new Color(1,1,1,0.99f));
-		
+		colorByIndex.add(new Color(1, 0, 0, 0.99f));
+		colorByIndex.add(new Color(0, 1, 1, 0.7f));
+		colorByIndex.add(new Color(1, 1, 1, 0.99f));
+
 		hex_grass = atlas.findRegion("grass");
 		hex_sea = atlas.findRegion("tile_sea");
 		hex_meadow = atlas.findRegion("tile_meadow");
@@ -80,30 +82,31 @@ public class InGame extends ScreenAdapter{
 		camera.lookAt(0, 0, 0);
 
 		Tile[][] map = gameMap.getMap();
-		
+
 		// load map
 		int width = gameMap.getMap().length;
 		int height = gameMap.getMap()[0].length;
-		
-		int originX = hex_grass.getRegionWidth()/2, originY = hex_grass.getRegionHeight()/2;
-		Stack<Actor> stackToDraw = new Stack<Actor>();	
-		
-		// for each tile create an Image actor 
+
+		int originX = hex_grass.getRegionWidth() / 2, originY = hex_grass
+				.getRegionHeight() / 2;
+		Stack<Actor> stackToDraw = new Stack<Actor>();
+
+		// for each tile create an Image actor
 		Image[][] tiles = new Image[width][height];
-		
+
 		final Clicked click = new Clicked();
 		Region someRegion = gameMap.getRegions(game.player).iterator().next();
-		
+
 		Tile occupying = someRegion.getTiles().iterator().next();
 		Unit u = new Unit(occupying);
 		occupying.occupant = u;
 		occupying.owner = game.player;
 		u.myVillage = someRegion.getVillage();
 		u.myType = UnitType.Peasant;
-		
+
 		gameMap.PrintPlayersStuff(game.player);
-		for (int y=0; y<width; y++) {
-			for (int x=0;x<height;x++) {
+		for (int y = 0; y < width; y++) {
+			for (int x = 0; x < height; x++) {
 				AtlasRegion hexToDraw;
 				if (map[x][y].getLandType() == LandType.Sea) {
 					hexToDraw = hex_sea;
@@ -116,150 +119,184 @@ public class InGame extends ScreenAdapter{
 				}
 				final Group tileGroup = new Group();
 				tileGroup.setOrigin(originX, originY);
-				tileGroup.setPosition(x*308*0.75f+150, x*88/2.0f + (y*88));
+				tileGroup.setPosition(x * 308 * 0.75f + 150, x * 88 / 2.0f
+						+ (y * 88));
 				final Image tile = new Image(hexToDraw);
 				tileGroup.addActor(tile);
 				if (map[x][y].containsVillage()) {
-					//Draw village on top of tile 
+					// Draw village on top of tile
 					final VillageImage village;
 					Village v = map[x][y].getVillage();
 					if (v.getType() == VillageType.Hovel) {
-						village= new VillageImage(village_hovel, v);
+						village = new VillageImage(village_hovel, v);
 					} else if (v.getType() == VillageType.Town) {
-						village= new VillageImage(village_town, v);
+						village = new VillageImage(village_town, v);
 					} else {
-						village= new VillageImage(village_fort, v);
+						village = new VillageImage(village_fort, v);
 					}
-					village.setPosition(200,44,0);
+					village.setPosition(200, 44, 0);
 					village.setOrigin(originX, originY);
 					final float screenX = x;
 					final float screenY = y;
 					village.addListener(new ClickListener() {
 						@Override
 						public void clicked(InputEvent event, float x, float y) {
-							Vector2 screenCoord = village.getStage().stageToScreenCoordinates(village.localToStageCoordinates(new Vector2(x,y)));
-							hud.villageClicked(village, screenCoord.x, screenCoord.y);
+							Vector2 screenCoord = village
+									.getStage()
+									.stageToScreenCoordinates(
+											village.localToStageCoordinates(new Vector2(
+													x, y)));
+							hud.villageClicked(village, screenCoord.x,
+									screenCoord.y);
 						}
 					});
 					tileGroup.addActor(village);
 				}
-//				System.out.println("Number of players:"+aGameRound.getPlayers().size());
+				// System.out.println("Number of players:"+aGameRound.getPlayers().size());
 
-				int playerNum = aGameRound.getPlayers().indexOf(map[x][y].getPlayer());			
+				int playerNum = aGameRound.getPlayers().indexOf(
+						map[x][y].getPlayer());
 				if (playerNum != -1) {
-					//If tile is owned by a player, tint it the corresponding colour
+					// If tile is owned by a player, tint it the corresponding
+					// colour
 					tile.setColor(colorByIndex.get(playerNum));
 				}
-//				tile.setPosition(x*tile.getWidth()*0.75f, x*tile.getHeight()/2.0f + (y*tile.getHeight()));
-				tileGroup.setPosition(x*308*0.75f, x*88/2.0f + (y*88));
-//				tile.setPosition(x*308*0.75f, x*88/2.0f + (y*88));
-//				tile.setOrigin(originX, originY);
+				// tile.setPosition(x*tile.getWidth()*0.75f,
+				// x*tile.getHeight()/2.0f + (y*tile.getHeight()));
+				tileGroup
+						.setPosition(x * 308 * 0.75f, x * 88 / 2.0f + (y * 88));
+				// tile.setPosition(x*308*0.75f, x*88/2.0f + (y*88));
+				// tile.setOrigin(originX, originY);
 				final Tile clickedTile = map[x][y];
-				final int X = x,Y = y;
+				final int X = x, Y = y;
 				tile.addListener(new ClickListener() {
 					@Override
 					public void clicked(InputEvent event, float x, float y) {
-						System.out.println("clicked on tile "+X+ " " + Y);
+						System.out.println("clicked on tile " + X + " " + Y);
 						hud.tileClicked(clickedTile);
-						if(click.isClicked()){
+						if (click.isClicked()) {
 							boolean good = true;
-							if(!gameMap.getNeighbouringTiles(click.potato.potato.myTile).contains(clickedTile)){
+							if (!gameMap.getNeighbouringTiles(
+									click.potato.potato.myTile).contains(
+									clickedTile)) {
+								good = false;
+							} else if (clickedTile.getUnit() != null
+									&& clickedTile
+											.getUnit()
+											.getType()
+											.compareTo(
+													click.potato.potato
+															.getType()) > 1) {
+								good = false;
+							} else if (clickedTile.getLandType().equals(
+									LandType.Sea)) {
 								good = false;
 							}
-							else if(clickedTile.getUnit() != null && 
-								clickedTile.getUnit().getType().compareTo(click.potato.potato.getType()) > 1  ){
-								good = false;
-							}
-							else if(clickedTile.getLandType().equals(LandType.Sea)){
-								good =false;
-							}
-							if(good && clickedTile.getLandType().equals(LandType.Tree)){
+							if (good
+									&& clickedTile.getLandType().equals(
+											LandType.Tree)) {
 								clickedTile.updateLandType(LandType.Grass);
 								click.potato.potato.getVillage().addWood(1);
-								tile.setDrawable(new TextureRegionDrawable(hex_grass));
+								tile.setDrawable(new TextureRegionDrawable(
+										hex_grass));
 							}
-							if(good && clickedTile.owner == null){
-								clickedTile.owner = click.potato.potato.getVillage().getOwner();
+							if (good && clickedTile.owner == null) {
+								clickedTile.owner = click.potato.potato
+										.getVillage().getOwner();
 								tile.setColor(colorByIndex.get(0));
 							}
-							if(good){
-								click.potato.potato.updateTileLocation(clickedTile);
+							if (good) {
+								click.potato.potato
+										.updateTileLocation(clickedTile);
 								tile.getParent().addActor(click.potato);
 							}
 							click.reset();
 						}
-						
+
 					}
 				});
-				
+
 				Unit potatosan = map[x][y].getUnit();
-				if(potatosan!=null && playerNum != -1){
+				if (potatosan != null && playerNum != -1) {
 					String colour = PotatoColours.values()[playerNum].colourName;
 					UnitType unitType = potatosan.getType();
 					String name = "";
-					if(unitType.equals(UnitType.Peasant)){
-						name = "potato_"+colour.toLowerCase();
+					if (unitType.equals(UnitType.Peasant)) {
+						name = "potato_" + colour.toLowerCase();
+					} else {
+						name = "potato_" + unitType.name().toLowerCase()
+								+ colour.toLowerCase();
 					}
-					else{
-						name = "potato_"+unitType.name().toLowerCase()+colour.toLowerCase();
-					}
-					System.out.println("Potato name " +name);
-					
-					Actor potatoImage = new PotatoImage(atlas.findRegion(name), potatosan, click);
-					tileGroup.addActor(potatoImage );
-					potatoImage.setPosition(50,40);
+					System.out.println("Potato name " + name);
+
+					Actor potatoImage = new PotatoImage(atlas.findRegion(name),
+							potatosan, click);
+					tileGroup.addActor(potatoImage);
+					potatoImage.setPosition(50, 40);
 				}
 				stackToDraw.push(tileGroup);
 			}
 		}
-		while (stackToDraw.size()>0) {
+		while (stackToDraw.size() > 0) {
 			gameStage.addActor(stackToDraw.pop());
 		}
 		// Stage
-		InputMultiplexer inputs = new InputMultiplexer(hud,gameStage);
+		InputMultiplexer inputs = new InputMultiplexer(hud, gameStage);
 		Gdx.input.setInputProcessor(inputs);
-		gameStage.addListener(new DragListener(){
-			float prevX , prevY;
+		gameStage.addListener(new DragListener() {
+			float prevX, prevY;
+
 			@Override
 			public void touchDragged(InputEvent event, float x, float y,
 					int pointer) {
 				super.touchDragged(event, x, y, pointer);
-				if(this.isDragging()){
-					gameStage.getCamera().position.add(prevX-x, prevY-y,0);
+				if (this.isDragging()) {
+					gameStage.getCamera().position.add(prevX - x, prevY - y, 0);
 				}
 			}
-			
+
+			@Override
+			public void dragStart(InputEvent event, float x, float y,
+					int pointer) {
+				super.dragStart(event, x, y, pointer);
+				prevX = x;
+				prevY = y;
+			}
 		});
-		gameStage.addListener(new InputListener(){
+		gameStage.addListener(new InputListener() {
 			@Override
 			public boolean keyDown(InputEvent event, int keycode) {
-				if(keycode == Keys.LEFT) gameStage.getCamera().position.add(-50, 0,0);
-				if(keycode == Keys.DOWN) gameStage.getCamera().position.add(0, -50,0);
-				if(keycode == Keys.RIGHT) gameStage.getCamera().position.add(50, 0,0);
-				if(keycode == Keys.UP) gameStage.getCamera().position.add(0,50,0);
+				if (keycode == Keys.LEFT)
+					gameStage.getCamera().position.add(-50, 0, 0);
+				if (keycode == Keys.DOWN)
+					gameStage.getCamera().position.add(0, -50, 0);
+				if (keycode == Keys.RIGHT)
+					gameStage.getCamera().position.add(50, 0, 0);
+				if (keycode == Keys.UP)
+					gameStage.getCamera().position.add(0, 50, 0);
 				return true;
 			}
-			
+
 		});
 	}
-	
-	public enum PotatoColours{
+
+	public enum PotatoColours {
 		PURPLE("purple"), RED("red"), YELLOW("yellow");
 		final public String colourName;
+
 		private PotatoColours(String colourName) {
 			this.colourName = colourName;
 		}
 	}
-	
-	
+
 	@Override
 	public void dispose() {
 		super.dispose();
 	}
+
 	@Override
 	public void show() {
 	}
-
 
 	@Override
 	public void render(float delta) {
@@ -268,23 +305,26 @@ public class InGame extends ScreenAdapter{
 		// update camera
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
-		
+
 		batch.begin();
 		gameStage.draw();
 		batch.end();
-		
+
 		hud.draw();
 	}
-	
+
 	class VillageImage extends Image {
 		public Village village;
+
 		public VillageImage(AtlasRegion region, Village village) {
 			super(region);
 			this.village = village;
 		}
+
 		public Village getVillage() {
 			return village;
 		}
+
 		public void updateVillageSprite() {
 			TextureRegionDrawable drawable;
 			if (village.getType() == VillageType.Hovel) {
@@ -297,45 +337,51 @@ public class InGame extends ScreenAdapter{
 			this.setDrawable(drawable);
 		}
 	}
-	
-	class PotatoImage extends Image{
+
+	class PotatoImage extends Image {
 		Unit potato;
 		Clicked click;
 		PotatoImage reference;
-		public PotatoImage(AtlasRegion region, final Unit potato, final Clicked click) {
+
+		public PotatoImage(AtlasRegion region, final Unit potato,
+				final Clicked click) {
 			super(region);
 			this.potato = potato;
 			this.click = click;
 			this.reference = this;
-			this.addListener(new ClickListener(){
+			this.addListener(new ClickListener() {
 				@Override
 				public void clicked(InputEvent event, float x, float y) {
-					System.out.println("Tickled on potato "+potato.getTile());
+					System.out.println("Tickled on potato " + potato.getTile());
 					click.clickedOn(reference);
 				}
 			});
 		}
 	}
-	
-	class Clicked{
+
+	class Clicked {
 		PotatoImage potato = null;
 		boolean isClicked = false;
-		
-		public void clickedOn(PotatoImage potato){
+
+		public void clickedOn(PotatoImage potato) {
 			this.potato = potato;
 			this.isClicked = true;
 		}
-		public boolean isClicked(){
+
+		public boolean isClicked() {
 			return this.isClicked;
 		}
-		public Unit potato(){
+
+		public Unit potato() {
 			return potato.potato;
 		}
-		public PotatoImage actor(){
+
+		public PotatoImage actor() {
 			return potato;
 		}
-		public void reset(){
-			this.potato=null;
+
+		public void reset() {
+			this.potato = null;
 			this.isClicked = false;
 		}
 	}
